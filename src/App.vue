45b55098ea4941b227d622bdd56905cd951f8389
carlosmab <carlosm.araujob@gmail.com>
2022-10-13 18:20:21 -0500

    Todos vuex module completed

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,24 @@
 <template>
   <div id="app">
     <div class="container">
+      <AddTodo />
+      <FilterTodos />
       <Todos />
     </div>
   </div>
 </template>
 
 <script>
-import Todos from "./components/Todos";
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
+import FilterTodos from './components/FilterTodos';
 
 export default {
   name: 'App',
   components: {
-    Todos
+    Todos,
+    AddTodo,
+    FilterTodos
   }
 }
 </script>
---
+++ src/components/Todos.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h3>Todos</h3>
+    <div class="todos">
+      <div v-for="todo in allTodos" :key="todo.id" class="todo">
+        {{ todo.title }}
+        <button @click="deleteTodo(todo.id)">
+          Borrar
+        </button> 
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex';
+
+export default {
+  name: 'Todos',
+  created() {
+    this.fetchTodos();
+  },
+  computed: mapGetters(['allTodos']),
+  methods: {
+    ...mapActions(['fetchTodos', 'deleteTodo'])
+  }
+}
+</script>
+
+<style scoped>
+
+
+</style>
